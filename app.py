c72507211f1e2fb68b72ae64a7d3e23eca50a746
sokilA <so.andriy98@gmail.com>
2019-12-17 01:25:11 +0200

    '1'

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm, RecaptchaField
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, Length, AnyOf
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Thisisasecret!'
+app.config['RECAPTCHA_PUBLIC_KEY'] = '6LfGCagUAAAAAD0uQ-L43NeIstEQ-MLYWkICdK9w'
+app.config['RECAPTCHA_PRIVATE_KEY'] = '6LfGCagUAAAAAEceXpOHrbwp_hYS2nE7gdPCBo9k'
+app.config['TESTING'] = True
+
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired('A username is required'),
+                                                   Length(min=5, max=10, message='Must be between 5 and 10')])
+    password = PasswordField('password', validators=[InputRequired('Password is required'),
+                                                     AnyOf(values=['password', 'secret'])])
+    recaptcha = RecaptchaField()
+
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        return '<h1> The username is {}. The password is {}.'.format(form.username.data, form.password.data)
+
+    return render_template('form.html', form=form)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
